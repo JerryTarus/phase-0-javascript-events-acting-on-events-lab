b05ef1c33224d07620a48d3a577a12a5cd7c40f5
Jerry Tarus <jerrytarus@gmail.com>
2023-10-07 17:13:24 +0300

    Completed Dodger Lab na wasiwasi

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+
+// Fetching the attribute by ID
+const dodger = document.getElementById("dodger");
+
+// Defining the Left Function
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+// Defining the Right Function
+const rightDodger = document.getElementById('dodger');
+function moveDodgerRight() {
+  const leftNumbers = rightDodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+        
+  if ( left > 0 ) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+      
+document.addEventListener('keydown', function(e) {
+  if ( e.key === "ArrowRight" ) {
+    moveDodgerRight();
+  }
+});
